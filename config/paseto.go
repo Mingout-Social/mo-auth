07c9e4198f4ed985fdabf7e7554f7625db2d5bc6
Nitish Kumar <nitishkr1729@gmail.com>
2022-10-13 17:10:52 +0530

    Read Keys from Env

--- config/paseto.go
+++ config/paseto.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"aidanwoods.dev/go-paseto"
+	"github.com/sirupsen/logrus"
+	"os"
 )
 
 const (
@@ -14,9 +16,19 @@ var PasetoSecretKey paseto.V4AsymmetricSecretKey
 var PasetoPublicKey paseto.V4AsymmetricPublicKey
 
 func InitPasetoPrivateKey() {
-	PasetoSecretKey = paseto.NewV4AsymmetricSecretKey()
+	var err error
+
+	PasetoSecretKey, err = paseto.NewV4AsymmetricSecretKeyFromHex(os.Getenv("PASETO_SECRET_KEY"))
+	if err != nil {
+		logrus.Panicf("Error Initializing Paseto Private Key, Err: %v", err)
+	}
 }
 
 func InitPasetoPublicKey() {
-	PasetoPublicKey = PasetoSecretKey.Public()
+	var err error
+
+	PasetoPublicKey, err = paseto.NewV4AsymmetricPublicKeyFromHex(os.Getenv("PASETO_PUBLIC_KEY"))
+	if err != nil {
+		logrus.Panicf("Error Initializing Paseto Public Key, Err: %v", err)
+	}
 }
